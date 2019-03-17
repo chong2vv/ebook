f01f3753840e007965df1722536451be780dee1f
chong2vv <chong2vv@163.com>
2019-03-17 16:42:11 +0800

    Initial Commit

---
+++ app_ios/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  app_ios
+//
+//  Created by 王远东 on 2019/3/17.
+//  Copyright © 2019 王远东. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ app_ios/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  app_ios
+//
+//  Created by 王远东 on 2019/3/17.
+//  Copyright © 2019 王远东. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
